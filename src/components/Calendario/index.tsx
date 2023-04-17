6ae038cdfe26e4aa5c783b7fc7f409922cf7c4d1
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2023-04-17 15:42:33 -0300

    atualiza evento após drag'n drop

--- src/components/Calendario/index.tsx
+++ src/components/Calendario/index.tsx
@@ -2,16 +2,19 @@
 import React from 'react'
 import style from './Calendario.module.scss';
 import ptBR from './localizacao/ptBR.json'
-import Kalend, { CalendarView } from 'kalend'
+import Kalend, { CalendarEvent, CalendarView, OnEventDragFinish } from 'kalend'
 import 'kalend/dist/styles/index.css';
-import { useRecoilValue } from 'recoil';
-import { listaDeEventosState } from '../../state/atom';
+import { IEvento } from '../../interfaces/IEvento';
+import useAtualizarEvento from '../../state/hooks/useAtualizarEvento';
+import useListaEventos from '../../state/hooks/useListaEventos';
 
+// startAt e endA são baseados em um Date com data e hora (ex. "2023-04-13T09:00")
 interface IKalendEvento {
   id?: number
   startAt: string
   endAt: string
   summary: string
+  completed: boolean
   color: string
 }
 
@@ -19,7 +22,8 @@ const Calendario: React.FC = () => {
 
   const eventosKalend = new Map<string, IKalendEvento[]>();
 
-  const eventos = useRecoilValue(listaDeEventosState);
+  const eventos = useListaEventos()
+  const atualizarEvento = useAtualizarEvento();
 
   eventos.forEach(evento => {
     const chave = evento.inicio.toISOString().slice(0, 10)
@@ -31,9 +35,29 @@ const Calendario: React.FC = () => {
       startAt: evento.inicio.toISOString(),
       endAt: evento.fim.toISOString(),
       summary: evento.descricao,
+      completed: evento.completo,
       color: 'blue'
     })
   })
+
+  // atualiza data e hora quando um evento é arrastado no calendário
+  const onEventDragFinish: OnEventDragFinish = (
+    prevEvent: CalendarEvent,
+    updatedEvent: CalendarEvent
+  ) => {
+    // este find não está funcionando quando novos eventos são adicionados
+    // listaEventos por algum motivo não é atualizada dentro dessa função
+    // const evento = listaEventos.find(item => item.id === updatedEvent.id)
+    const eventoAlterado : IEvento = {
+      id: updatedEvent.id,
+      inicio: new Date(updatedEvent.startAt),
+      fim: new Date(updatedEvent.endAt),
+      descricao: updatedEvent.summary,
+      completo: updatedEvent.completed
+    }
+    atualizarEvento(eventoAlterado);    
+  };
+
   return (
     <div className={style.Container}>
       <Kalend
@@ -46,6 +70,7 @@ const Calendario: React.FC = () => {
         calendarIDsHidden={['work']}
         language={'customLanguage'}
         customLanguage={ptBR}
+        onEventDragFinish={onEventDragFinish}
       />
     </div>
   );
